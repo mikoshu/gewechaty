6e1b3062d24e4523be82a56efee60c4f7e30d5bc
Eric <eric.wong.cug@gmail.com>
2024-11-27 17:46:56 +0000

    async 移除显式promise，修改getXml2Json驼峰拼写getXmlToJson

--- src/class/MESSAGE.js
+++ src/class/MESSAGE.js
@@ -10,11 +10,15 @@ import { MessageType } from '@/type/MessageType'
 
 // 消息类
 export class Message {
+  // 静态属性
+  static Type = MessageType
+  // 实例属性
   constructor(data) {
     // 从 JSON 数据结构中提取所需信息
-    this.fromId = data.Data.FromUserName.string
-    this.toId = data.Data.ToUserName.string
-    this.isRoom = data.Data.FromUserName.string.endsWith('@chatroom')
+    this.wxid = data.Wxid;
+    this.fromId = data.Data.FromUserName.string;
+    this.toId = data.Data.ToUserName.string;
+    this.isRoom = data.Data.FromUserName.string.endsWith('@chatroom');
     this._msgId = data.Data.MsgId || null;
     this._newMsgId = data.Data.NewMsgId || null;
     this._text = data.Data.Content.string || '';
@@ -26,90 +30,80 @@ export class Message {
     this._msgSeq = data.Data.MsgSeq || null;
     this._status = data.Data.Status || null;
     this._msgSource = data.Data.MsgSource || null;
+
     if (this.isRoom) { // 执行一次 自动插入房间数据
-      getRoomInfo(this.fromId)
+      getRoomInfo(this.fromId);
     }
   }
-  // 静态属性
-  static Type = MessageType
+
   // 实例方法
   isCompanyMsg () { // 是否是企业微信消息
     const companyList = ['weixin', 'newsapp', 'tmessage', 'qqmail', 'mphelper', 'qqsafe', 'weibo', 'qmessage', 'floatbottle', 'medianote']
     return this.fromId.includes('gh_') || companyList.includes(this.fromId)
   }
-  from () { // 发送者
+  //发送者
+  from () {
     if (this.isRoom) {
       return getContact(this._text.split(':\n')[0])
     }
     return getContact(this.fromId);
   }
+  //发送者
   talker () {
     if (this.isRoom) {
       return getContact(this._text.split(':\n')[0])
     }
     return getContact(this.fromId);
   }
-
-  to () { // 接收者
+  //接收者
+  to () {
     return getContact(this.toId);
   }
-
-  async room () { // 是否是群聊消息 是则返回群信息
-    if (this.isRoom) {
-      return await getRoomInfo(this.fromId)
-    } else {
-      return Promise.resolve(false)
+  // 是否是群聊消息 是则返回群信息
+  async room () {
+    if (!this.isRoom) return false;
+    if (!this._roomInfo) {
+      this._roomInfo = await getRoomInfo(this.fromId);
     }
+    return this._roomInfo;
   }
-
-  text () { // 消息内容
-    if (this.fromId.endsWith('@chatroom')) {
+  // 消息内容
+  text () {
+    if (this.isRoom) {
       return this._text.split(':\n').slice(1).join(':\n')
     }
     return this._text;
   }
-
-  async say (textOrContactOrFileOrUrl) { // 回复消息
+  // 发送消息
+  async say (textOrContactOrFileOrUrl) {
     const res = await say(textOrContactOrFileOrUrl, this.fromId)
     return new ResponseMsg(res)
   }
-
-  type () { // 消息类型
+  // 消息类型
+  type () {
     return Message.getType(this._type, this.text())
   }
-
-  self () { // 是否是自己发的消息
+  // 是否是自己发的消息
+  self () {
     return this._self;
   }
-
-  async mention () { // 获取@的联系人 ..todo
-    return new Promise((resolve) => {
-      // 根据消息内容模拟提到的联系人
-      console.log('暂不支持')
-      resolve(null);
-    });
+  // 获取@的联系人 ..todo
+  async mention () {
+    // 根据消息内容模拟提到的联系人
+    console.log('暂不支持')
+    return null;
   }
-
-  async mentionSelf () { // 是否被@了
-
-    if (this.isRoom && this._msgSource) {
-      const parser = new XMLParser({
-        ignoreAttributes: false,
-        parseAttributeValue: true,
-        trimValues: true
-      });
-      const result = parser.parse(this._msgSource);
-      if (result.msgsource?.atuserlist) {
-        const atUserList = result.msgsource.atuserlist;
-        if (Array.isArray(atUserList) && atUserList.includes(this.Wxid)) {
-          return true;
-        }
-      }
+  // 是否被@了
+  async mentionSelf () {
+    if (!this.isRoom || !this._msgSource) {
+      return false;
     }
-    return false;
+    const result = this.getXmlToJson(this._msgSource);
+    const atUserList = result.msgsource.atuserlist;
+    return (Array.isArray(atUserList) && atUserList.includes(this.wxid))
   }
-
-  async forward (to) { // 消息转发
+  // 消息转发
+  async forward (to) {
     if (!to) {
       console.error('转发消息时，接收者不能为空')
       return
@@ -122,49 +116,42 @@ export class Message {
   }
 
   age () {
-    const now = new Date();
+    const now = Date.now();
     return Math.floor((now - this._date) / 1000); // 以秒为单位计算消息的年龄
   }
-
-  async toContact () { // 获取名片 。。。todo
-    return new Promise((resolve) => {
-      console.log('暂不支持')
-      resolve(null);
-    });
+  // 获取名片 。。。todo
+  async toContact () {
+    console.log('暂不支持')
+    return null;
   }
-
-  async toUrlLink () { // 获取链接。。。todo
-    return new Promise((resolve) => {
-      console.log('暂不支持')
-      resolve(null);
-    });
+  // 获取链接。。。todo
+  async toUrlLink () {
+    console.log('暂不支持')
+    return null;
   }
-
-  async toFileBox (type = 2) { // 获取链接。。。todo
+  // 获取图片。。。todo
+  async toFileBox (type = 2) {
     if (this._type !== 3) {
-      console.log('不是图片类型，无法调用toFileBox方法')
+      console.log('不是图片类型，无法调用toFileBox方法');
+      return null;
+    }
+    let xml = ''
+    if (this.isRoom) {
+      xml = this._text.split(":\n")[1]
+    } else {
+      xml = this._text
+    }
+    try {
+      const url = await toFileBox(xml, type);
+      return Filebox.toDownload(url)
+    } catch (e) {
+      console.error(e)
       return null
     }
-    return new Promise((resolve) => {
-      let xml = ''
-      if (this.isRoom) {
-        xml = this._text.split(":\n")[1]
-      } else {
-        xml = this._text
-      }
-      toFileBox(xml, type).then((url) => {
-        resolve(Filebox.toDownload(url))
-      }).catch(e => {
-        console.error(e)
-        resolve(null)
-      })
-    });
   }
-  async toFilebox (type = 2) {
-    return this.toFileBox(type)
-  }
-  async quote (title) { // 引用消息
-    if (title === '') {
+  // 引用消息
+  async quote (title) {
+    if (!title || title === '') {
       console.error('引用消息时title不能为空')
       return
     }
@@ -179,7 +166,8 @@ export class Message {
     }
     return quote(msg, this.fromId)
   }
-  getXml2Json (xml) {
+  // 获取xml转json
+  static getXmlToJson (xml) {
     const parser = new XMLParser({
       ignoreAttributes: false, // 不忽略属性
       attributeNamePrefix: '', // 移除默认的属性前缀
@@ -198,81 +186,82 @@ export class Message {
   }
 
   static getType (type, xml) {
-    let parser, jObj
+    let jObj
     try {
       switch (type) {
         case 1:
-          return MessageType.Text
+          return MessageType.Text;
+          break;
         case 3:
-          return MessageType.Image
+          return MessageType.Image;
+          break;
         case 34:
-          return MessageType.Voice
+          return MessageType.Voice;
+          break;
         case 37:
-          return MessageType.AddFriend
+          return MessageType.AddFriend;
+          break;
         case 42:
-          return MessageType.Contact
+          return MessageType.Contact;
+          break;
         case 43:
-          return MessageType.Video
+          return MessageType.Video;
+          break;
         case 47:
-          return MessageType.Emoji
+          return MessageType.Emoji;
+          break;
         case 48:
           return MessageType.Location
+          break;
         case 49:
-          parser = new XMLParser({
-            ignoreAttributes: false, // 不忽略属性
-            attributeNamePrefix: '', // 移除默认的属性前缀
-          });
-          jObj = parser.parse(xml);
+          jObj = getXmlToJson(xml);
           // console.log(jObj)
           if (jObj.msg.appmsg.type === 5) {
             if (jObj.msg.appmsg.title === '邀请你加入群聊') {
               return MessageType.RoomInvitation
             } else { // 公众号链接
-              return MessageType.Link
+              return MessageType.Link;
             }
           } else if (jObj.msg.appmsg.type === 6) {
-            return MessageType.File
+            return MessageType.File;
           } else if (jObj.msg.appmsg.type === 17) {
-            return MessageType.RealTimeLocation
+            return MessageType.RealTimeLocation;
           } else if (jObj.msg.appmsg.type === 19) {
-            return MessageType.ChatHistroy
+            return MessageType.ChatHistroy;
           } else if (jObj.msg.appmsg.type === 33 || jObj.msg.appmsg.type === 36) {
-            return MessageType.MiniApp
+            return MessageType.MiniApp;
           } else if (jObj.msg.appmsg.type === 51) {
-            return MessageType.VideoAccount
+            return MessageType.VideoAccount;
           } else if (jObj.msg.appmsg.type === 57) {
-            return MessageType.Quote
+            return MessageType.Quote;
           } else if (jObj.msg.appmsg.type === 74) {
-            return MessageType.FileStart
+            return MessageType.FileStart;
           } else if (jObj.msg.appmsg.type === 2000) {
-            return MessageType.Transfer
+            return MessageType.Transfer;
           } else if (jObj.msg.appmsg.type === 2001) {
-            return MessageType.RedPacket
+            return MessageType.RedPacket;
           }
+          break;
         case 50:
-        //VOIP挂断
+          //VOIP挂断
+          break;
         case 51:
           //状态通知
-          parser = new XMLParser({
-            ignoreAttributes: false, // 不忽略属性
-            attributeNamePrefix: '', // 移除默认的属性前缀
-          });
-          jObj = parser.parse(xml);
+          jObj = getXmlToJson(xml);
           if (jObj.msg.name === 'MomentsTimelineStatus') {
             //新的朋友圈消息
           } else if (jObj.msg.name === 'lastMessage') {
             //群聊消息
           }
+          break;
         case 56:
-        //语音群聊
+          //语音群聊
+          break;
         case 10000:
-        //系统消息
+          //群通知
+          break;
         case 10002:
-          parser = new XMLParser({
-            ignoreAttributes: false, // 不忽略属性
-            attributeNamePrefix: '', // 移除默认的属性前缀
-          });
-          jObj = parser.parse(xml);
+          jObj = getXmlToJson(xml);
           if (jObj.sysmsg.type === 'revokemsg') {
             return MessageType.Revoke
           } else if (jObj.sysmsg.type === 'pat') {
@@ -285,16 +274,14 @@ export class Message {
           } else if (jObj.sysmsg.type === 'trackmsg') {
             //实时位置更新
           }
+          break;
         default:
           return MessageType.Unknown
       }
     } catch (e) {
       return MessageType.Unknown
     }
-
-
   }
-
 }
 
 export class ResponseMsg {
